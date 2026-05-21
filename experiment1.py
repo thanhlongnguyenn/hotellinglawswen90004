04229f45b79f99b264528d1d3a14fc2c4de7b62f
joshua-m-vaughan <joshvaughan132@gmail.com>
2026-05-21 21:48:42 +1000

    feat: isolate log utilities for reuse

--- experiment1.py
+++ experiment1.py
@@ -1,51 +1,5 @@
-import csv
-from datetime import datetime
+import log_utils
 from simulation import Simulation
-
-
-def create_table_version_2_0_log_file(experiment_name: str):
-    """Create a CSV file that is compatible with NetLogo Table Version 2.0.
-
-    Returns:
-        Tuple of (csv_file, csv_writer) where csv_file is the opened file object and
-        csv_writer is the csv.writer object for writing to the file.
-    """
-
-    curr_time: datetime = datetime.now()
-
-    # Create file and writer.
-    filepath = f"{curr_time.strftime('%Y%m%d-%H%M%S')}_{experiment_name}.csv"
-    csv_file = open(filepath, mode="w", newline="")
-    csv_writer = csv.writer(csv_file)
-
-    # Write header row.
-    csv_writer.writerow(["Python Complex Model results", "Table version 2.0"])
-    csv_writer.writerow(["Simulation.py"])
-    csv_writer.writerow([experiment_name])
-    csv_writer.writerow([f"{curr_time.isoformat()}"])
-
-    return csv_file, csv_writer
-
-def dump_list_to_table_version_2_0_format(a_list) -> str:
-    """Convert a list to a string format compatible with NetLogo Table Version 2.0.
-
-    Example:
-        [[1, 2, 3], 2, 3] -> [[1 2 3] 2 3]
-    """
-    first_elem: bool = True
-    out = "["
-
-    for a in a_list:
-        if isinstance(a, list):
-            out += dump_list_to_table_version_2_0_format(a)
-        else:
-            if (not first_elem):
-                out += " "
-            out += str(a)
-            first_elem = False
-
-    out += "]"
-    return out
 
 
 def experiment_one():
@@ -58,7 +12,7 @@ def experiment_one():
     """
 
     # Initialise test logging file.
-    csv_file, csv_writer = create_table_version_2_0_log_file("experiment1")
+    csv_file, csv_writer = log_utils.create_log_file("experiment1")
     csv_writer.writerow(["min-pxcor", "max-pxcor", "min-pycor", "max-pycor"])
     csv_writer.writerow(["-20", "20", "-20", "20"])
     csv_writer.writerow(
@@ -101,21 +55,24 @@ def experiment_one():
                     )
 
                     # Log results to CSV.
-                    state:dict = sim.export_state()
-                    csv_writer.writerow([
-                        run_counter,
-                        layout,
-                        num_store,
-                        rule,
-                        state["step"],
-                        dump_list_to_table_version_2_0_format(state["store-positions"]),
-                        dump_list_to_table_version_2_0_format(state["store-market-shares"]),
-                        dump_list_to_table_version_2_0_format(state["store-prices"]),
-                    ])
+                    state: dict = sim.export_state()
+                    csv_writer.writerow(
+                        [
+                            run_counter,
+                            layout,
+                            num_store,
+                            rule,
+                            state["step"],
+                            log_utils.serialise_list(state["store-positions"]),
+                            log_utils.serialise_list(state["store-market-shares"]),
+                            log_utils.serialise_list(state["store-prices"]),
+                        ]
+                    )
     except KeyboardInterrupt:
         pass
     finally:
         csv_file.close()
+
 
 if __name__ == "__main__":
     experiment_one()
---
+++ log_utils.py
@@ -0,0 +1,50 @@
+"""
+Table Version 2.0 logging utilities.
+"""
+
+import csv
+from datetime import datetime
+
+def create_log_file(experiment_name: str):
+    """Create a CSV file that is compatible with NetLogo Table Version 2.0.
+
+    Returns:
+        Tuple of (csv_file, csv_writer) where csv_file is the opened file object and
+        csv_writer is the csv.writer object for writing to the file.
+    """
+
+    curr_time: datetime = datetime.now()
+
+    # Create file and writer.
+    filepath = f"{curr_time.strftime('%Y%m%d-%H%M%S')}_{experiment_name}.csv"
+    csv_file = open(filepath, mode="w", newline="")
+    csv_writer = csv.writer(csv_file)
+
+    # Write header row.
+    csv_writer.writerow(["Python Complex Model results", "Table version 2.0"])
+    csv_writer.writerow(["Simulation.py"])
+    csv_writer.writerow([experiment_name])
+    csv_writer.writerow([f"{curr_time.isoformat()}"])
+
+    return csv_file, csv_writer
+
+def serialise_list(a_list) -> str:
+    """Convert a list to a string format compatible with NetLogo Table Version 2.0.
+
+    Example:
+        [[1, 2, 3], 2, 3] -> [[1 2 3] 2 3]
+    """
+    first_elem: bool = True
+    out = "["
+
+    for a in a_list:
+        if isinstance(a, list):
+            out += serialise_list(a)
+        else:
+            if (not first_elem):
+                out += " "
+            out += str(a)
+            first_elem = False
+
+    out += "]"
+    return out
