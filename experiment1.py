3366861ce41a63bb40f4f003db811d0e3844b9f3
joshua-m-vaughan <joshvaughan132@gmail.com>
2026-05-22 08:47:36 +1000

    fix: remove ticks from csv fie header

--- experiment1.py
+++ experiment1.py
@@ -26,7 +26,6 @@ if __name__ == "__main__":
             "number-of-stores",
             "rules",
             "[step]",
-            "ticks",
             "store-positions",
             "store-market-shares",
             "store-prices",
