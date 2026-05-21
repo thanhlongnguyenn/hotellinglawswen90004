eca42a86d06aa377886078e6f29d0e9c19929161
joshua-m-vaughan <joshvaughan132@gmail.com>
2026-05-21 22:14:02 +1000

    feat: parallelised experiment 1 test case runner

--- experiment1.py
+++ experiment1.py
@@ -1,8 +1,48 @@
+import multiprocessing
 import log_utils
 from simulation import Simulation
 
 
-def experiment_one():
+def experiment_one(params):
+    """Experiment 1 test case runner for multiprocessing.
+
+    Args:
+        params (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    run_id, max_ticks, num_store, rule, layout = params
+
+    # Build simulation environment.
+    sim = Simulation(
+        number_of_stores=num_store,
+        rules=rule,
+        layout=layout,
+    )
+
+    # Execute test iteration.
+    for _ in range(max_ticks):
+        sim.step()
+    print(f"Completed: Num stores: {num_store}, Layout: {layout}, Rule: {rule}")
+
+    # Build results.
+    state: dict = sim.export_state()
+    results = [
+        run_id,
+        layout,
+        num_store,
+        rule,
+        state["step"],
+        log_utils.serialise_list(state["store-positions"]),
+        log_utils.serialise_list(state["store-market-shares"]),
+        log_utils.serialise_list(state["store-prices"]),
+    ]
+    return results
+
+
+if __name__ == "__main__":
     """EXPERIMENT 1: Python Model Validation
 
     Number of stores: [2, 3, ..., 9, 10]
@@ -30,49 +70,29 @@ def experiment_one():
     )
 
     # Define test iterations.
+    max_ticks: int = 500
     number_of_stores: range = range(2, 10, 1)
     geographic_space: set = {"line", "plane"}
     rules: set = {"normal", "pricing-only", "moving-only"}
-    max_ticks: int = 500
+
+    # Build test cases
+    test_case: list[tuple] = []
+    run_counter: int = 1
+    for num_store in number_of_stores:
+        for layout in geographic_space:
+            for rule in rules:
+                test_case.append((run_counter, max_ticks, num_store, layout, rule))
 
     try:
-        run_counter: int = 1
-        for num_store in number_of_stores:
-            for layout in geographic_space:
-                for rule in rules:
-                    # Build simulation environment.
-                    sim = Simulation(
-                        number_of_stores=num_store,
-                        rules=rule,
-                        layout=layout,
-                    )
+        # Run test cases on multiple cores.
+        with multiprocessing.Pool() as pool:
+            results = list(pool.imap_unordered(experiment_one, test_case))
 
-                    # Execute test iteration.
-                    for _ in range(max_ticks):
-                        sim.step()
-                    print(
-                        f"Completed: Num stores: {num_store}, Layout: {layout}, Rule: {rule}"
-                    )
+        # Write logs to files
+        for result in results:
+            csv_writer.writerow(result)
 
-                    # Log results to CSV.
-                    state: dict = sim.export_state()
-                    csv_writer.writerow(
-                        [
-                            run_counter,
-                            layout,
-                            num_store,
-                            rule,
-                            state["step"],
-                            log_utils.serialise_list(state["store-positions"]),
-                            log_utils.serialise_list(state["store-market-shares"]),
-                            log_utils.serialise_list(state["store-prices"]),
-                        ]
-                    )
     except KeyboardInterrupt:
         pass
     finally:
         csv_file.close()
-
-
-if __name__ == "__main__":
-    experiment_one()
